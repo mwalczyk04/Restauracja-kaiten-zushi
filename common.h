48d8b1203c18f84bf70ffdca34d773fba72c3e09
mwalczyk04 <michal.walczyk04@gmail.com>
2025-12-31 11:59:01 +0100

    Stworzenie zarysu struktur w pliku common.h

---
+++ common.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <errno.h>
+
+#define MAX_STOLIKI 20
+#define MAX_TASMA 20
+#define ID_PROJEKT 'V'
+
+typedef struct {
+	int rodzaj; // Kolor talerza , moze zmienic potem 
+	int cena;
+	int rezerwacja_dla;  
+}Talerz;
+
+typedef struct {
+	int id;
+	int pojemnosc;
+	int kto_zajmuje;
+	int ile_osob_siedzi;
+}Stolik;
+
+typedef struct {
+	Talerz tasma[MAX_TASMA];
+	Stolik stoliki[MAX_STOLIKI];
+	int czy_otwarte;
+	int utarg;
+}Restauracja;
